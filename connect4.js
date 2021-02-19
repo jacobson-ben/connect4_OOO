a047dc2acbae8c86bdf5d2f9d4aedccb5567e295
Ben Jacobson <jacobson.ben11@gmail.com>
2021-02-19 15:12:01 -0500

    first updates

--- connect4.js
+++ connect4.js
@@ -5,21 +5,23 @@
  * board fills (tie)
  */
 
-const WIDTH = 7;
-const HEIGHT = 6;
+class Game {
+  constructor(HEIGHT, WIDTH) {
+    this.HEIGHT = HEIGHT;
+    this.WIDTH = WIDTH;
+    this.makeBoard()
+    this.makeHtmlBoard()
+    this.currPlayer = 1
+  }
 
-let currPlayer = 1; // active player: 1 or 2
-let board = []; // array of rows, each row is array of cells  (board[y][x])
-
-/** makeBoard: create in-JS board structure:
- *   board = array of rows, each row is array of cells  (board[y][x])
- */
-
-function makeBoard() {
-  for (let y = 0; y < HEIGHT; y++) {
-    board.push(Array.from({ length: WIDTH }));
+  makeBoard() {
+    this.board = [];
+    for (let y = 0; y < HEIGHT; y++) {
+      board.push(Array.from({length: WIDTH}));
+    }
   }
 }
+
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
